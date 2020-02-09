d0bccd72f3fee0919be2e671c456ab625dac2692
zhifang.wang <1948904329@qq.com>
2020-02-10 02:57:15 +0800

    update

--- dubbo-order-service/src/main/java/com/sunny/dubbo/service/impl/OrderServiceImpl.java
+++ dubbo-order-service/src/main/java/com/sunny/dubbo/service/impl/OrderServiceImpl.java
@@ -31,7 +31,7 @@ import javax.annotation.Resource;
 public class OrderServiceImpl implements OrderService {
 
     @Resource
-    UserService userService;
+    private UserService userService;
 
     @Override
     public void initOrder(Integer userId) {
